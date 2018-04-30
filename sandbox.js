726fbed5d8ae1c160843ab6784f8d085f69e18fa
klrqn <burtonsb@gmail.com>
2018-04-30 23:35:55 +0000

    add and change todo added -v2

--- sandbox.js
+++ sandbox.js
@@ -5,3 +5,35 @@ var todos = ['1', '2', 3, 4, 5];
 todos.push('6');
 todos.splice(0, 1);
 
+// TODOs for todo list:
+// v2
+// display items
+// add items
+// change items
+// delete items
+var listOfTodos = [1,2,3,4,5];
+
+function displayTodos() {
+	console.log('my Todos: ', listOfTodos);
+}
+
+displayTodos();
+
+function addTodo(todo) {
+	console.log(`executing addTodo(${todo})...`);
+	listOfTodos.push(todo);
+	displayTodos();
+}
+
+addTodo(6);
+
+function changeTodo(oldTodo, newTodo) {
+	for (var i=0; i<listOfTodos.length; i++) {
+		if (listOfTodos[i] == oldTodo) {
+			listOfTodos[i] = newTodo;
+		}
+	}
+	displayTodos();
+}
+
+changeTodo(6, 7);
